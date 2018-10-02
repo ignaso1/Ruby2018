163238dd4f5b1755a9cd2e94192ccb845be57dd4
Ignas <ignasobulaitis@gmail.com>
2018-10-02 18:52:28 +0300

    company rubocop fixed

--- company.rb
+++ company.rb
@@ -1,51 +1,54 @@
+# Company class
 class Company
   def initialize(role_type = 'base')
     @information = { company_id: nil,
                      is_running: false,
-					 debt: 0,
-					 resources_count: 0,
-					 ceo_name: nil,
-					 ceo_id: nil,
-					 has_ceo: false
-					 }
+                     debt: 0,
+                     resources_count: 0,
+                     ceo_name: nil,
+                     ceo_id: nil,
+                     has_ceo: false }
     @information[:role_type] = role_type
   end
-  
+
   def register(company_id, company_name = nil)
     @information[:company_id] = company_id
     @information[:company_name] = company_name
-	@information[:is_running] = true
+    @information[:is_running] = true
   end
-  
-  def unregister()
-	@information[:is_running] = false
+
+  def unregister
+    @information[:is_running] = false
   end
-  
-  def fireCEO()
-	@information[:has_ceo] = false
+
+  def fire_ceo
+    @information[:has_ceo] = false
   end
-  
+
   def registered?
     return true unless @information.fetch(:company_id).equal?(nil)
+
     false
   end
-  
-  def placeCEO(ceo_id, ceo_name = nil)
+
+  def place_ceo(ceo_id, ceo_name = nil)
     @information[:ceo_id] = ceo_id
     @information[:ceo_name] = ceo_name
   end
-  
-  def hasCEO?
-	return true unless @information.fetch(:has_ceo).equal?(true)
+
+  def ceo?
+    return true unless @information.fetch(:has_ceo).equal?(true)
+
     false
   end
-  
+
   def information(symbol)
     @information.fetch(symbol.to_sym)
   end
-  
+
   def bankrupt?
-	return true unless @information.fetch(:debt) < 0
+    return true unless @information.fetch(:debt) < 0
+
     false
   end
 end
