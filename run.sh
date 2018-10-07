61a91787a8584ca2c61cdbca48dbe2e5fc82708a
Ignas <ignasobulaitis@gmail.com>
2018-10-07 22:01:39 +0300

    changed run.sh classes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 bundle install
-bundle exec mutant --include lib --use rspec Match
+bundle exec mutant --include lib --use rspec Admin Comment Company LeaveNotice PotentialResource Project ProjectManager Resource User
 bundle exec rspec
 bundle exec rubocop
 bundle exec reek
