a0743b0c294a0f478367a593e6612ce18f7d39d0
Ignas <ignasobulaitis@gmail.com>
2018-12-11 14:55:01 +0200

    run.sh modified

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 bundle install
-bundle exec mutant --u rspec User ProjectManager Project
+bundle exec mutant --u rspec Admin Ceo Comment Company HumanResourceManager LeaveNotice PotentialResource Resource User ProjectManager Project
 bundle exec rspec
 bundle exec rubocop
 bundle exec reek
