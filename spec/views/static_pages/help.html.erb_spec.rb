63c8e3fa6bf3e4a102768ce3d1a8502312a6f330
Karolis <karolis.vie@gmail.com>
2018-12-18 15:56:58 +0200

    Views and specs

--- spec/views/static_pages/help.html.erb_spec.rb
+++ spec/views/static_pages/help.html.erb_spec.rb
@@ -3,5 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe 'static_pages/help.html.erb', type: :view do
+  context 'displays requirements correctly' do
+    before :each do
+      render
+    end
 
+    it 'Name' do
+      expect(rendered).to match /Name should be up to 50 alphanumerical characters/
+    end
+
+    it 'Last Name' do
+      expect(rendered).to match /Last Name should be up to 50 alphanumerical characters/
+    end
+
+    it 'Role' do
+      expect(rendered).to match /Role shold be one of : Admin, Project manager, Human resource manager or CEO/
+    end
+
+    it 'Email' do
+      expect(rendered).to match /Email should look like : email@email.com/
+    end
+
+    it 'Password' do
+      expect(rendered).to match /Password shouldn't be less than 8 characters/
+    end
+  end 
 end
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe 'static_pages/home.html.erb', type: :view do
+  context 'when logged in' do
+    before do
+      allow(view).to receive(:logged_in?).and_return(true) # Mock
+    end
 
-end
+    it 'links to profile if logged in' do
+      render
+      expect(rendered).to match /Profile/
+    end
+  end
+
+  context 'when not logged in' do
+    before do
+      allow(view).to receive(:logged_in?).and_return(false) # Mock
+    end
+
+    it 'links to sign up' do
+      render
+      expect(rendered).to match /Sign up/
+    end
+  end
+end 
